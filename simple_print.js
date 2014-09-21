a8f8bb5bb2f550d07f00d039bfcc6ac7ac82fecb
Conrad <conrad.watt12@imperial.ac.uk>
2014-09-21 15:25:54 +0100

    Implement extra flags at parser level.

--- simple_print.js
+++ simple_print.js
@@ -4,7 +4,9 @@ var filename = process.argv[2];
 fs.readFile(filename, "utf8", f);
 
 function f(err, data) {
-  var out = JSON.stringify(esprima.parse(data, {range: true}), null, 4);
+  var strict = (process.argv.indexOf("-force_strict") >= 0);
+  var init = (process.argv.indexOf("-builtin_init") >= 0);
+  var out = JSON.stringify(esprima.parse(data, {range: true, builtin_init: init, force_strict: strict}), null, 4);
   var output = filename.substring(0, (filename.length - 3)) + ".json";
   fs.writeFile(output, out, [], null);
 }
