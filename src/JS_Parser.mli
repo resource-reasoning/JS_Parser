9d66b1977cb1cb04227a9d8f7e0bdff8b34ebdd6
Sacha-Elie Ayoun <sacha-elie.ayoun@supelec.fr>
2019-04-08 16:26:55 +0100

    doc: fix doc errors and copy in source

--- src/JS_Parser.mli
+++ src/JS_Parser.mli
@@ -6,15 +6,32 @@ module PrettyPrint : module type of struct
   include PrettyPrint
 end
 
-module Error : module type of struct
-  include Error
+module Error : sig
+  type t =
+  | Unknown_Annotation of string
+      (** There was an unkown JS_Logic annotation *)
+  | Overlapping_Syntax
+      (** Something went wrong with the parser, some syntax is overlapping *)
+  | Unhandled_Statement of int
+      (** The statement at the given offset is not handled. Maybe because it is not part of ES5 *)
+  | Unhandled_Expression of int
+      (** The expression at the given offset is not handled. Maybe because it is not part of ES5 *)
+  | NotEcmaScript5 of string * int
+      (** Something used in the script is not part of ES5 *)
+  | UnusedAnnotations of string list * int
+      (** Some JS_Logic annotations were in the wrong place *)
+  | FlowParser of string  (** Some Error happened at the flow_parser lever. *)
+
+  val str : t -> string
+
+  exception ParserError of t
 end
 
 val parse_string_exn : ?force_strict:bool -> string -> Syntax.exp
-(** [parse_string_exn ~force_strict prog] the given string as a program.
+(** [parse_string_exn ~force_strict prog] parses the given string as a program.
     The string given should be the entire program.
-    If [force_strict] is true, the program has to be strict.
-    If there is an error during the parsing, an exception of type [Error.ParserError] is raised *)
+    If [force_strict] is true, the program has to be strict. Default value for [force_strict] is [false].
+    If there is an error during the parsing, an exception of type {!Error.ParserError} is raised *)
 
 val parse_string : ?force_strict:bool -> string -> (Syntax.exp, Error.t) result
 (** Same as [parse_string_exn] except that it returns a result instead of raising an error. *)
