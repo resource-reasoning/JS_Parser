8cd0ea188548df89e7da82da57f6cce1b2b9f627
pmaksimo <p.maksimovic@imperial.ac.uk>
2020-01-05 12:31:57 +0100

    lalala

--- scripts/JSIL_add_promises_header.py
+++ scripts/JSIL_add_promises_header.py
@@ -410,6 +410,34 @@ function checkSettledPromises(settleds, expected, message) {
   });
 }
 
+function compareArray(a, b) {
+  if (b.length !== a.length) {
+    return false;
+  }
+
+  for (var i = 0; i < a.length; i++) {
+    if (!compareArray.isSameValue(b[i], a[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+compareArray.isSameValue = function(a, b) {
+  if (a === 0 && b === 0) return 1 / a === 1 / b;
+  if (a !== a && b !== b) return true;
+
+  return a === b;
+};
+
+assert.compareArray = function(actual, expected, message) {
+  var format = compareArray.format;
+  assert(
+    compareArray(actual, expected),
+    'Expected ${format(actual)} and ${format(expected)} to have the same contents. ${(message || '')}'
+  );
+};
+
 """
 
 
